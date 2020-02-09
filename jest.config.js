fb10c061971298e805c5e59ba24681743b0d9a0f
Samuel Imolorhe <samuelimolo4real@gmail.com>
2020-02-09 02:33:50 +0100

    Added travis configuration.

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!**/node_modules/**',
+  ],
+  coverageDirectory: '.coverage',
+};
